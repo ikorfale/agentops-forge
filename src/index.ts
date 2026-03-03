3faf815d7b62f4ce8c032367a4b012a4efb6213c
ikorfale <82613188+ikorfale@users.noreply.github.com>
2026-03-03 08:05:32 +0400

    feat: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./commands/discover.js";
+export * from "./commands/outreach.js";
+export * from "./commands/social.js";
+export * from "./commands/guard.js";
+export * from "./commands/receipt.js";
+export * from "./commands/handoff.js";
